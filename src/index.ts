9036365ab8055a9232341ee4e956685c96e50fb8
Ashish Sharda <ashishjsharda@gmail.com>
2022-07-24 16:12:48 -0500

    update index ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-console.clear();
-console.log("Ashish's TODO list");
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+let todos = [
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
